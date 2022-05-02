21d460ad2cdcccec98fcbd6a5150ee599298ec01
Jonathan Tran <digitalsigna@gmail.com>
2022-05-02 09:34:09 -0700

    added inquirer, console.table, mysql to package; added server.js and schema with file structures

---
+++ db/server.js
@@ -0,0 +1,13 @@
+const MySql = require('mysql2');
+
+const connection = mysql.createConnection( {
+    
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "hrtracking_db",
+      },
+      console.log("Server is Live")
+    );
+    
+    module.exports = connection;
